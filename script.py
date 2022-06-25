423f0df675fae0b525b7ed69eff4c4885a092952
Umair Ali <56534384+eeumairali@users.noreply.github.com>
2022-06-25 15:22:50 +0500

    update for multiple formats of website

--- script.py
+++ script.py
@@ -7,7 +7,9 @@ import pandas as pd
 import os
 
 #write path
-path_input = "/home/umair/Desktop/FilteringCsvFiles/inputCsvFiles/"
+#path_input = "/home/umair/Desktop/FilteringCsvFiles/inputCsvFiles/"
+path_input = "inputCsvFiles/"
+
 #path_output = "/home/umair/Desktop/FilteringCsvFiles/FilteredOutputFile/"
 list_of_all_files = os.listdir(path_input)
 
@@ -19,12 +21,13 @@ for filename in list_of_all_files:
         all_input_csvs.append(filename)
 
 final_csv = pd.DataFrame()
+keyword = "dermologykit.com"
 for filename in all_input_csvs:
     print("working on file",filename)
     current_data = pd.read_csv(path_input+filename, encoding='unicode_escape')
     # applying filtering
     url_column = current_data.LeadURL
-    filtered_data = current_data[(url_column == "dermologykit.com")]    
+    filtered_data = current_data[ url_column.str.contains(keyword) ]    
     final_csv = pd.concat([final_csv, filtered_data],axis=0)
 
 final_csv.to_csv("final.csv")    
